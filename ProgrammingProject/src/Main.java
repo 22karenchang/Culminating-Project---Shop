2d3c1128b4e84281901e91884ec851f053874a82
karen <349960997@gapps.yrdsb.ca>
2025-01-09 13:34:33 -0500

    change variables to arraylists, finished all three categories inside UserMode, store item names, quantities, and prices into 3 separate arraylists

--- ProgrammingProject/src/Main.java
+++ ProgrammingProject/src/Main.java
@@ -1,5 +1,6 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.*;
+
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
@@ -20,93 +21,195 @@ public class Main {
             if (password.equals("Admin's/Password:)")) {
                 System.out.println("You have successfully logged in!");
             }
-        } else if (mode == 2){ //user mode
-            //set up for shop
-            int total = 0;
-            int sneakers_available = 4;
-            int sneakers_price = 105;
-            int boots_available = 6;
-            int boots_price = 200;
-            int sandals_available = 3;
-            int sandals_price = 45;
-            int lipsticks_available = 4;
-            int lipsticks_price = 25;
-            int eyeliners_available = 5;
-            int eyeliners_price = 8;
-            int foundations_available = 2;
-            int foundations_price = 30;
-            int earrings_available = 6;
-            int earrings_price = 16;
-            int sunglasses_available = 5;
-            int sunglasses_price = 190;
-            int hats_available = 3;
-            int hats_price = 13;
+        } else if (mode == 2) { //user mode
+            //set up for User Mode
+            String[] categories = {"Footwear", "Makeup", "Accessories"};
+            String[] products_1 = {"Sneakers", "Boots", "Sandals"};
+            String[] products_2 = {"Lipsticks", "Eyeliners", "Foundations"};
+            String[] products_3 = {"Earrings", "Sunglasses", "Hats"};
+            int[] prices_1 = {105, 200, 45};
+            int[] prices_2 = {25, 8, 30};
+            int[] prices_3 = {16, 190, 13};
+            int[] stock_1 = {4, 6, 3};
+            int[] stock_2 = {4, 5, 2};
+            int[] stock_3 = {6, 5, 3};
+            int totalPrice = 0;
+            int quantity = 0;
+            int item = 0;
+            ArrayList<String> cartItems = new ArrayList<String>(); //can you have multiple arrays inside?
+            ArrayList<Integer> itemQuantities = new ArrayList<>(); // all quantity stored here
+            ArrayList<Integer> itemTotalPrice = new ArrayList<>(); // each item's total stored here
+            //each index of each arraylist will represent an item added to cart
+
             System.out.println("Hi, User!");
-            System.out.println("Here is our three categories, please select: " +
-                    "1. Footwear  2. Makeup  3. Accessories");
-            int category = input.nextInt();
-            if (category == 1) { //footwear
-                System.out.println("Here is our menu for Footwear: ");
-                System.out.println("1. Sneakers\t$105\t4 available");
-                System.out.println("2. Boots\t$200\t6 available");
-                System.out.println("3. Sandals\t$45 \t3 available");
-                System.out.println("Select the product: ");
-                int product = input.nextInt();
-                if (product == 1) { //sneakers
-                    System.out.println("Select quantity: ");
-                    int quantity = input.nextInt();
-                } else if (product == 2) { //boots
-                    System.out.println("Select quantity: ");
-                    int quantity = input.nextInt();
-                } else if (product == 3) { //sandals
-                    System.out.println("Select quantity: ");
-                    int quantity = input.nextInt();
-                } else {
-                    System.out.println("Invalid product");
-                }
-            } else if (category == 2) { //makeup
-                System.out.println("Here is our menu for Makeup: ");
-                System.out.println("1. Lipsticks\t$25\t4 available");
-                System.out.println("2. Eyeliners\t$8\t5 available");
-                System.out.println("3. Foundations\t$30\t2 available");
-                System.out.println("Select the product: ");
-                int product = input.nextInt();
-                if (product == 1) { //lipsticks
-                    System.out.println("Select quantity: ");
-                    int quantity = input.nextInt();
-                } else if (product == 2) { //eyeliners
-                    System.out.println("Select quantity: ");
-                    int quantity = input.nextInt();
-                } else if (product == 3) { //foundations
-                    System.out.println("Select quantity: ");
-                    int quantity = input.nextInt();
-                } else {
-                    System.out.println("Invalid product");
-                }
-            } else if (category == 3) { //accessories
-                System.out.println("Here is our menu for Accessories: ");
-                System.out.println("1. Earrings \t$16 \t6 available");
-                System.out.println("2. Sunglasses\t$190\t5 available");
-                System.out.println("3. Hats       \t$13 \t3 available");
-                System.out.println("Select the product: ");
-                int product = input.nextInt();
-                if (product == 1) { //earrings
-                    System.out.println("Select quantity: ");
-                    int quantity = input.nextInt();
-                } else if (product == 2) { //sunglasses
-                    System.out.println("Select quantity: ");
-                    int quantity = input.nextInt();
-                } else if (product == 3) { //hats
-                    System.out.println("Select quantity: ");
-                    int quantity = input.nextInt();
-                } else {
-                    System.out.println("Invalid product");
-                }
-            } else {
-                System.out.println("Invalid category");
+            for (int i = 0; i < categories.length; i++) {
+                System.out.println("Here is our three categories, please select: " + categories[i]);
             }
-        } else { //invalid
-            System.out.println("Invalid mode");
+            int chosenCategory = input.nextInt() - 1; //index!!!
+            if (chosenCategory >= 0 && chosenCategory < categories.length) {
+                switch (chosenCategory) {
+                    case 0: //footwear
+                        for (int i = 0; i < products_1.length; i++) { //menuForProducts in each category
+                            System.out.println(products_1[i] + "\t$" + prices_1[i] + "\t" + stock_1[i]
+                                    + " available");
+                        }
+                        System.out.println("Select an item");
+                        item = input.nextInt() - 1; //INDEX!!
+                        switch (item) {
+                            case 0: //sneaker
+                                System.out.println("Select quantity: ");
+                                quantity = input.nextInt();
+                                if (quantity <= stock_1[0] && quantity > 0) {
+                                    cartItems.add(products_1[item]);
+                                    itemQuantities.add(quantity);
+                                    itemTotalPrice.add(prices_1[item] * quantity); //may have issues!
+                                } else {
+                                    System.out.println("Quantity exceeds stock limit " +
+                                            "OR Quantity can't be 0");
+                                    quantity = input.nextInt(); //enter again?
+                                }
+                                break;
+                            case 1: //boots
+                                System.out.println("Select quantity: ");
+                                quantity = input.nextInt();
+                                if (quantity <= stock_1[1] && quantity > 0) {
+                                    cartItems.add(products_1[item]);
+                                    itemQuantities.add(quantity);
+                                    itemTotalPrice.add(prices_1[item] * quantity); //may have issues!
+                                } else {
+                                    System.out.println("Quantity exceeds stock limit " +
+                                            "OR Quantity can't be 0");
+                                    quantity = input.nextInt(); //enter again?
+                                }
+                                break;
+                            case 2: //sandals
+                                System.out.println("Select quantity: ");
+                                quantity = input.nextInt();
+                                if (quantity <= stock_1[2] && quantity > 0) {
+                                    cartItems.add(products_1[item]);
+                                    itemQuantities.add(quantity);
+                                    itemTotalPrice.add(prices_1[item] * quantity); //may have issues!
+                                } else {
+                                    System.out.println("Quantity exceeds stock limit " +
+                                            "OR Quantity can't be 0");
+                                    quantity = input.nextInt(); //enter again?
+                                }
+                                break;
+                            default:
+                                System.out.println("invalid item number");
+                                break;
+                        }
+                        break;
+                    case 1: // makeup
+                        for (int i = 0; i < products_2.length; i++) { //print items in category2
+                            System.out.println(products_2[i] + "\t$" + prices_2[i] + "\t" + stock_2[i]
+                                    + " available");
+                        }
+                        System.out.println("Select an item");
+                        item = input.nextInt() - 1;
+                        switch (item) {
+                            case 0: //lipsticks
+                                System.out.println("Select quantity: ");
+                                quantity = input.nextInt();
+                                if (quantity <= stock_2[0] && quantity > 0) {
+                                    cartItems.add(products_2[item]);
+                                    itemQuantities.add(quantity);
+                                    itemTotalPrice.add(prices_2[item] * quantity); //may have issues!
+                                } else {
+                                    System.out.println("Quantity exceeds stock limit " +
+                                            "OR Quantity can't be 0");
+                                    quantity = input.nextInt(); //enter again?
+                                }
+                                break;
+                            case 1: //eyeliners
+                                System.out.println("Select quantity: ");
+                                quantity = input.nextInt();
+                                if (quantity <= stock_2[1] && quantity > 0) {
+                                    cartItems.add(products_2[item]);
+                                    itemQuantities.add(quantity);
+                                    itemTotalPrice.add(prices_2[item] * quantity); //may have issues!
+                                } else {
+                                    System.out.println("Quantity exceeds stock limit " +
+                                            "OR Quantity can't be 0");
+                                    quantity = input.nextInt(); //enter again?
+                                }
+                                break;
+                            case 2: //foundations
+                                System.out.println("Select quantity: ");
+                                quantity = input.nextInt();
+                                if (quantity <= stock_2[1] && quantity > 0) {
+                                    cartItems.add(products_2[item]);
+                                    itemQuantities.add(quantity);
+                                    itemTotalPrice.add(prices_2[item] * quantity); //may have issues!
+                                } else {
+                                    System.out.println("Quantity exceeds stock limit " +
+                                            "OR Quantity can't be 0");
+                                    quantity = input.nextInt(); //enter again?
+                                }
+                                break;
+                            default:
+                                System.out.println("invalid item number");
+                                break;
+                        }
+
+                    case 2: //accessories
+                        for (int i = 0; i < products_3.length; i++) {
+                            System.out.println(products_3[i] + "\t$" + prices_3[i] + "\t" + stock_3[i]
+                                    + " available");
+                        }
+                        System.out.println("Select an item");
+                        item = input.nextInt() - 1;
+                        switch (item) {
+                            case 0: //earrings
+                                System.out.println("Select quantity: ");
+                                quantity = input.nextInt();
+                                if (quantity <= stock_3[0] && quantity > 0) {
+                                    cartItems.add(products_3[item]);
+                                    itemQuantities.add(quantity);
+                                    itemTotalPrice.add(prices_3[item] * quantity); //may have issues!
+                                } else {
+                                    System.out.println("Quantity exceeds stock limit " +
+                                            "OR Quantity can't be 0");
+                                    quantity = input.nextInt(); //enter again?
+                                }
+                                break;
+                            case 1: //sunglasses
+                                System.out.println("Select quantity: ");
+                                quantity = input.nextInt();
+                                if (quantity <= stock_3[1] && quantity > 0) {
+                                    cartItems.add(products_3[item]);
+                                    itemQuantities.add(quantity);
+                                    itemTotalPrice.add(prices_3[item] * quantity); //may have issues!
+                                } else {
+                                    System.out.println("Quantity exceeds stock limit " +
+                                            "OR Quantity can't be 0");
+                                    quantity = input.nextInt(); //enter again?
+                                }
+                                break;
+                            case 2: //hats
+                                System.out.println("Select quantity: ");
+                                quantity = input.nextInt();
+                                if (quantity <= stock_3[1] && quantity > 0) {
+                                    cartItems.add(products_3[item]);
+                                    itemQuantities.add(quantity);
+                                    itemTotalPrice.add(prices_3[item] * quantity); //may have issues!
+                                } else {
+                                    System.out.println("Quantity exceeds stock limit " +
+                                            "OR Quantity can't be 0");
+                                    quantity = input.nextInt(); //enter again?
+                                }
+                                break;
+                            default:
+                                System.out.println("invalid item number");
+                                break;
+                        }
+
+
+                        break; //break for category
+
+                }
+            } else
+                System.out.println("Invalid category");
         }
     }
 }
